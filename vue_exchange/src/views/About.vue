3a19e67c204ca51647aac07703c5e9383f71cf49
gnuxdar <gnuxdar@gmail.com>
2021-01-20 21:54:22 -0500

    componentes de vistas

---
+++ vue_exchange/src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex-col items-center">
+        <h1 class="text-gray-700 text-6xl">Vue Exchange v1.0.0</h1>
+        <p class="text-gray-600 text-xl text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit accusantium officia pariatur deleniti quasi voluptatum porro ut dolorum, tempore sapiente provident iusto distinctio maxime. Architecto nulla aspernatur similique fugiat illo!
+        </p>
+
+        <router-link 
+            to="/"
+            class="mt-5 text-xl text-green-600 hover:underline">Inicio</router-link>
+    </div>
+</template>
---
+++ vue_exchange/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <art-assets-table  v-bind:assets="assets" />
+    </div>
+</template>
+
+<script>
+import api from '@/api'
+import ArtAssetsTable from '@/components/ArtAssetsTable.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        ArtAssetsTable,
+    },
+
+    data(){
+        return {
+            assets: []
+        }
+    },
+
+    created() {
+        api.getAssets()
+            .then(assets => (this.assets = assets))
+    }
+}
+</script>
