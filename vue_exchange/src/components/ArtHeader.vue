bd0843dc36eeb5ab1a0ad774b6518e170b754c9e
gnuxdar <gnuxdar@gmail.com>
2021-01-20 21:56:26 -0500

    importat el componente de el logo

--- vue_exchange/src/components/ArtHeader.vue
+++ vue_exchange/src/components/ArtHeader.vue
@@ -3,7 +3,12 @@
         <nav>
             <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
                 <div class="flex items-center flex-shrink-0 text-white mr-6">
+                    <art-my-icon  class="mr-2"/>
                     <span class="font-semibold text-xl tracking-tight">VUE Exchange</span>
+                    <span class="ml-2">
+                        <router-link
+                        to="about">About</router-link>
+                    </span>
                 </div>
                 <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div class="text-sm lg:flex-grow"></div>
@@ -12,3 +17,12 @@
         </nav>
     </header>
 </template>
+
+<script>
+import ArtMyIcon from '@/components/ArtMyIcon.vue'
+export default {
+    name: 'ArtHeader',
+    components: { ArtMyIcon }
+
+}
+</script>
