e9cf7bb6395fd488c97366a3b6a82e1270025ebc
gnuxdar <gnuxdar@gmail.com>
2021-01-20 21:55:03 -0500

    importando los filtros

--- vue_exchange/src/main.js
+++ vue_exchange/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
+
+//para poder utilizar Vue.filter
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
