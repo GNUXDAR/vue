268d4cd28e7023de54873d257abbe59f79194fc8
gnuxdar <gnuxdar@gmail.com>
2021-01-23 13:41:52 -0500

    add new functions

--- vue_exchange/src/api.js
+++ vue_exchange/src/api.js
@@ -1,17 +1,20 @@
 const url = 'https://api.coincap.io/v2'
 
+//para obetner las 20 principales criptomonedas
 function getAssets(){
     return fetch(`${url}/assets?limit=20`)
     .then(res => res.json())
     .then(res => res.data)
 }
 
+//para obtener una coin en particular
 function getAsset(coin){
     return fetch(`${url}/assets/${coin}`)
     .then(res => res.json())
     .then(res => res.data)
 }
 
+//para obtener el historial de la coin
 function getAssetHistory(coin) {
     const now = new Date()
     const end = now.getTime()
@@ -23,8 +26,23 @@ function getAssetHistory(coin) {
     .then(res => res.data)
 }
 
+function getMarkets(coin) {
+    return fetch(`${url}/assets/${coin}/markets?limit=7`)
+    .then(res => res.json())
+    .then(res => res.data)
+}
+
+function getExchange(id) {
+    return fetch(`${url}/exchanges/${id}`)
+    .then(res => res.json())
+    .then(res => res.data)
+}
+
+//exportar funcion
 export default {
     getAssets,
     getAsset,
-    getAssetHistory
+    getAssetHistory,
+    getMarkets,
+    getExchange
 }
