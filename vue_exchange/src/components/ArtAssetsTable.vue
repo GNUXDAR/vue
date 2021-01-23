8fdb06eaca13176d4a1bde6d5f4866d989ea4fd4
gnuxdar <gnuxdar@gmail.com>
2021-01-23 13:43:55 -0500

    add new componen ArtButton

--- vue_exchange/src/components/ArtAssetsTable.vue
+++ vue_exchange/src/components/ArtAssetsTable.vue
@@ -28,7 +28,14 @@
                         <b>#{{ a.rank }}</b>
                     </td>
                     <td>
-                        {{ a.name }}
+                        <router-link
+                            class="hover:underline text-green-600"
+                            :to="{ name: 'coin-detail', params: { id: a.id}}">
+                            {{ a.name }}
+                        </router-link>
+                        <small class="ml-1 text-gray-500">
+                            {{ a.symbol }}
+                        </small>
                     </td>
                     <td>
                         {{ a.priceUsd | dollar}}
@@ -39,20 +46,35 @@
                     <td :class="a.changePercent24Hr.includes('-') ? 'text-red-600' : 'text-gree-600'">
                         {{ a.changePercent24Hr | percent }}
                     </td>
-                    <td class="hidden sm:block"></td>
+                    <td class="hidden sm:block">
+                        <art-button @custom-click = "goToCoin(a.id)">
+                            <span>Detalle</span>
+                        </art-button>
+                    </td>
             </tr>
         </tbody>
     </table>
 </template>
 
 <script>
+import ArtButton from '@/components/ArtButton'
+
 export default {
     name: 'ArtAssetsTable',
+    components: {
+        ArtButton
+    },
     props: {
         assets: {
             type: Array,
             default: ()=> []
         }
+    },
+
+    methods: {
+       goToCoin(id) { //navegacion programatica con $router en el boton componenete ArtButton
+           this.$router.push({ name: 'coin-detail', params: { id }})
+       } 
     }
 }
 </script>
