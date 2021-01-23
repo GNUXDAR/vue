760705a0d099a0512fb6675a13795449b7bad18f
gnuxdar <gnuxdar@gmail.com>
2021-01-23 13:42:45 -0500

    add new plugins vue-spinner and chart

--- vue_exchange/src/main.js
+++ vue_exchange/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import '@/assets/css/tailwind.css'
 import router from '@/router'
 import { dollarFilter, percentFilter } from '@/filters'
+import Chart from 'chart.js'  //necesaria para el loader de vue-spiner
+import Chartick from 'vue-chartkick'
+import { VueSpinners} from '@saeris/vue-spinners'
+
+//para poder utilizar componente de terceros Vue.use
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
 
 //para poder utilizar Vue.filter
 Vue.filter('dollar', dollarFilter)
