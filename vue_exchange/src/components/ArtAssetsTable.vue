bb793c19140412c38ce868d6e61f5007edbedcc3
gnuxdar <gnuxdar@gmail.com>
2021-01-28 01:40:02 -0500

    add filter with input

--- vue_exchange/src/components/ArtAssetsTable.vue
+++ vue_exchange/src/components/ArtAssetsTable.vue
@@ -3,19 +3,28 @@
         <thead>
             <tr class="bg-gray-100 border-b-2 border-gray-400">
                 <th></th>
-                <th>
-                    <span>Ranking</span>
+                <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+                    <span
+                        class = "underline cursor-pointer"
+                        @click = "changeSortOrder">Ranking</span>
                 </th>
                 <th>Nombre</th>
                 <th>Precio</th>
                 <th>Cap de mercado</th>
                 <th>Variacion 24 hrs</th>
-                <td class="hidden sm:block"></td>
+                <td class="hidden sm:block">
+                    <input class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal" 
+                    id="filter"
+                    placeholder="Buscar..."
+                    type="text"
+                    v-model="filter"
+                    >
+                </td>
             </tr>
         </thead>
         <tbody>
             <tr 
-                v-for="a in assets"
+                v-for="a in filteredAssets"
                 v-bind:key ="a.id"
                 class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
             >
@@ -64,21 +73,72 @@ export default {
     components: {
         ArtButton
     },
+    data() {
+        return {
+            filter: '',
+            sortOrder: 1,
+        }
+    },
     props: {
         assets: {
             type: Array,
             default: ()=> []
         }
     },
+    computed: {
+        filteredAssets() { //para buscar por el symbol o por el nombre de la cryptomoneda
+
+            const altOrder = this.sortOrder == 1 ? -1 : 1
+
+            return this.assets.filter(
+                a=> a.symbol.toLowerCase().includes(this.filter.toLowerCase()) || 
+                a.name.toLowerCase().includes(this.filter.toLowerCase())
+            )
+            .sort((a, b) =>  {
+                if(parseInt(a.rank) > parseInt(b.rank)){
+                    return this.SortOrder
+                }
+
+                return altOrder
+            })
+        }
+    },
 
     methods: {
        goToCoin(id) { //navegacion programatica con $router en el boton componenete ArtButton
            this.$router.push({ name: 'coin-detail', params: { id }})
-       } 
+       },
+       changeSortOrder() {  //ordenar el ranking
+            this.sortOrder = this.sortOrder === 1 ? -1 : 1
+       }
     }
 }
 </script>
 
 <style scoped>
-
+.up::before {
+  content: '👆';
+}
+.down::before {
+  content: '👇';
+}
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+  th {
+    padding: 12px;
+  }
+}
 </style>
